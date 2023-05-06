cd84db8cee16923be59c5e7ae2163b15ae13c14b
백승훈 <bagzaru@naver.com>
2023-05-06 17:28:52 +0900

    JGitManager created

--- src/main/java/GitManager/GitManager.java
+++
@@ -1,6 +0,0 @@
-package main.java.GitManager;
-
-public class GitManager {
-
-
-}
---
+++ src/main/java/jgitmanager/JGitManager.java
@@ -0,0 +1,9 @@
+package main.java.gitmanager;
+
+public class JGitManager {
+    public static void main(String args[]) {
+
+    }
+
+
+}
