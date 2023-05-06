c09f9c4b7f6fac8ac1fa4a76094fe818463230c0
백승훈 <bagzaru@naver.com>
2023-05-06 23:03:35 +0900

    package name modified (error modified)

--- src/main/java/jgitmanager/JGitManager.java
+++ src/main/java/jgitmanager/JGitManager.java
@@ -1,9 +1,7 @@
-package main.java.gitmanager;
+package main.java.jgitmanager;
 
 public class JGitManager {
     public static void main(String args[]) {
-
+        System.out.println("Hello World!");
     }
-
-
 }
