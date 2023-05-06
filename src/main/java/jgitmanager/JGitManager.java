4b3d3126db4aa1aa1e63ada446a1a8ab0cd42b45
백승훈 <bagzaru@naver.com>
2023-05-06 23:14:12 +0900

    JGitUIBridge created, JGitManager comment addedj

--- src/main/java/jgitmanager/JGitManager.java
+++ src/main/java/jgitmanager/JGitManager.java
@@ -1,7 +1,9 @@
 package main.java.jgitmanager;
 
+//JGit의 기능을 직접 사용하여 Git init, add, ... 등으로 단순화하는 클래스입니다.
+//git의 특정 명령어를 직접 사용하려면 이 클래스를 사용합니다.
 public class JGitManager {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World from JGitManager");
     }
 }
