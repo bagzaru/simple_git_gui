a9b6367a015eba986ea8642221d46c12f0071b08
SaeJoon <psae0714@naver.com>
2023-05-13 12:06:32 +0900

    button finish panel switch case finish

--- src/main/java/gui/GitFilePanel.java
+++ src/main/java/gui/GitFilePanel.java
@@ -4,12 +4,38 @@ import file.SelectedFile;
 import gitmenu.modified_fileGitMenu;
 import gitmenu.staged_fileGitMenu;
 import gitmenu.untracked_fileGitMenu;
+import jgitmanager.*;
 
 import javax.swing.*;
 
 public class GitFilePanel extends JPanel {
     public GitFilePanel(){
         removeAll();
+        if(JGitManager.findGitRepository(SelectedFile.getInstance().getFile())==1){
+        switch(SelectedFile.getInstance().getGitStatus()){
+            case FOLDER :
+                break;
+            case UNTRACKED :
+                add(new untracked_fileGitMenu());
+                break;
+            case MODIFIED :
+                add(new modified_fileGitMenu());
+                break;
+            case STAGED_MODIFIED :
+                add(new modified_fileGitMenu());
+                break;
+            case STAGED :
+                add(new staged_fileGitMenu());
+                break;
+            case UNMODIFIED :
+                add(new staged_fileGitMenu());
+                break;
+            case DELETED :
+                break;     
+            default :
+                break;              
+        }
+    }
 /*
         switch(SelectedFile.getInstance().getGitStatus()){
             case 0 :
