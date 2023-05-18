7565ae71257cad9727cb9174e2f0c688caf6cdc9
keylime7 <keylime7@cau.ac.kr>
2023-05-18 15:09:57 +0900

    delete jgituibridge package

--- src/main/java/jgituibridge/JGitUIBridge.java
+++
@@ -1,9 +0,0 @@
-package jgituibridge;
-
-//JGitManager의 기능을 정제하여 제작된 함수입니다.
-//Button 등으로 호출할 때 JGitManager를 직접 사용하지 않고, 이 클래스를 통해 소통합니다.
-public class JGitUIBridge {
-    public static void main(String[] args){
-        System.out.println("Hello World from JGitUIBridge");
-    }
-}
