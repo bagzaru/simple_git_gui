523e92661c16e0f65d62c060fff595ab178d5cb6
SaeJoon <psae0714@naver.com>
2023-05-26 22:28:10 +0900

    Branch Rename & Merge button

--- src/main/java/gui/branchpanel/component/branch/button/GitBranchMergeButton.java
+++ src/main/java/gui/branchpanel/component/branch/button/GitBranchMergeButton.java
@@ -1,6 +1,17 @@
 package gui.branchpanel.component.branch.button;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GitBranchMergeButton extends JButton {
+    GitBranchMergeButton(){
+        setText("M");
+        addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
