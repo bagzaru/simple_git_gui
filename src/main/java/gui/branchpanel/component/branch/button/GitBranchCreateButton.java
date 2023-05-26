d3aaa464e81edb5ce3f8fc1253460dc54128436c
SaeJoon <psae0714@naver.com>
2023-05-26 22:24:03 +0900

    Branch create button

--- src/main/java/gui/branchpanel/component/branch/button/GitBranchCreateButton.java
+++ src/main/java/gui/branchpanel/component/branch/button/GitBranchCreateButton.java
@@ -1,6 +1,18 @@
 package gui.branchpanel.component.branch.button;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GitBranchCreateButton extends JButton {
+    GitBranchCreateButton(){
+        setText("+");
+        //messageBox 브랜치 이름
+        addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
