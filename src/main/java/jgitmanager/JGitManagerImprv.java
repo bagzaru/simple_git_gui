3fdc1e9de4f11e5981b43e95ed45468cc47bfde2
min-jp <mincjiip@gmail.com>
2023-05-29 16:56:12 +0900

    add exception handling for gitCommitInfo method

--- src/main/java/jgitmanager/JGitManagerImprv.java
+++ src/main/java/jgitmanager/JGitManagerImprv.java
@@ -201,12 +201,17 @@ public class JGitManagerImprv {
     // gitCommitInfo
     // 특정 commit의 정보를 리턴합니다
     // CommitInfo 객체를 리턴합니다.
-    public static CommitInfo gitCommitInfo(RevCommit nowCommit) {
-        return new CommitInfo(nowCommit.getId().getName(),
-                nowCommit.getCommitTime(),
-                nowCommit.getFullMessage(),
-                nowCommit.getAuthorIdent().getName(),
-                nowCommit.getAuthorIdent().getEmailAddress());
+    public static CommitInfo gitCommitInfo(RevCommit nowCommit) throws Exception {
+        try {
+            return new CommitInfo(nowCommit.getId().getName(),
+                    nowCommit.getCommitTime(),
+                    nowCommit.getFullMessage(),
+                    nowCommit.getAuthorIdent().getName(),
+                    nowCommit.getAuthorIdent().getEmailAddress());
+        } catch (Exception e) {
+            System.out.println("Failed to get commit information");
+            throw e;
+        }
     }
 
     public static void gitChangedFileList() {
