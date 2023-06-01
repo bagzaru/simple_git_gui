d495d4762b4b2c751bf660f917a84a6f03c4df25
SaeJoon <psae0714@naver.com>
2023-06-01 09:48:15 +0900

    pull project2

--- src/main/java/gui/branchpanel/component/commit/GitCommitStatus.java
+++ src/main/java/gui/branchpanel/component/commit/GitCommitStatus.java
@@ -1,7 +1,14 @@
 package gui.branchpanel.component.commit;
 
+import file.GitBranchData;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class GitCommitStatus extends JPanel {
+    private GitBranchData gitBranchData;
+    public GitCommitStatus(){
+        this.setLayout(new GridLayout(5,2));
+    }
 
 }
