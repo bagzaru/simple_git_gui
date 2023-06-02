4bde764d7768907673e114600d48979ee3a5778c
SaeJoon <psae0714@naver.com>
2023-06-02 12:24:14 +0900

    RenameButton finish

--- src/main/java/gui/branchpanel/component/branch/button/GitBranchRenameButton.java
+++ src/main/java/gui/branchpanel/component/branch/button/GitBranchRenameButton.java
@@ -1,10 +1,17 @@
 package gui.branchpanel.component.branch.button;
 
+import file.GitBranchData;
+import file.SelectedFile;
+import jgitmanager.JGitManagerImprv;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class GitBranchRenameButton extends JButton {
+    private GitBranchData gitBranchData;
     public GitBranchRenameButton(){
         setText("R");
         MessageBox RenameBranchName=new MessageBox();//messageBox new branch name 추가 예정
@@ -13,6 +20,14 @@ public class GitBranchRenameButton extends JButton {
             public void actionPerformed(ActionEvent e) {
 
                 String NewBranchName=RenameBranchName.ShowMessageBox();
+                try {
+                    JGitManagerImprv.gitRenameBranch(SelectedFile.getInstance().getFile(),
+                            gitBranchData.getSelectedBranch(),NewBranchName);
+                } catch (GitAPIException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
     }
