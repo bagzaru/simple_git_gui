1dba137e053b05c90814eb42ec9a40d9805c4407
SaeJoon <psae0714@naver.com>
2023-06-02 12:19:45 +0900

    DeleteButton finish

--- src/main/java/gui/branchpanel/component/branch/button/GitBranchDeleteButton.java
+++ src/main/java/gui/branchpanel/component/branch/button/GitBranchDeleteButton.java
@@ -1,16 +1,30 @@
 package gui.branchpanel.component.branch.button;
 
+import file.GitBranchData;
+import file.SelectedFile;
+import jgitmanager.JGitManagerImprv;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class GitBranchDeleteButton extends JButton {
+    private GitBranchData gitBranchData;
     public GitBranchDeleteButton(){
         setText("-");
         addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                try {
+                    JGitManagerImprv.gitDeleteBranch(SelectedFile.getInstance().getFile(),
+                            gitBranchData.getSelectedBranch());
+                } catch (GitAPIException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
     }
