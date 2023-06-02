253b2a2da9a9350ef31846ce45a890186a451224
SaeJoon <psae0714@naver.com>
2023-06-02 12:28:37 +0900

    MergeButton finish

--- src/main/java/gui/branchpanel/component/branch/button/GitBranchMergeButton.java
+++ src/main/java/gui/branchpanel/component/branch/button/GitBranchMergeButton.java
@@ -1,16 +1,30 @@
 package gui.branchpanel.component.branch.button;
 
+import file.GitBranchData;
+import file.SelectedFile;
+import jgitmanager.JGitManagerImprv;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class GitBranchMergeButton extends JButton {
+    private GitBranchData gitBranchData;
     public GitBranchMergeButton(){
         setText("M");
         addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                try {
+                    JGitManagerImprv.gitMerge(SelectedFile.getInstance().getFile(),
+                            gitBranchData.getSelectedBranch());
+                } catch (GitAPIException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
     }
