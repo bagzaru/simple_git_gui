c0eb114cd05847f18ae7d98675b809440a493b0e
SaeJoon <psae0714@naver.com>
2023-06-02 16:10:41 +0900

    3.1 panel finish

--- src/main/java/gui/branchpanel/component/branch/GitBranchStatus.java
+++ src/main/java/gui/branchpanel/component/branch/GitBranchStatus.java
@@ -9,21 +9,18 @@ public class GitBranchStatus extends JPanel {
     /** Branch 모드 공유 데이터 객체*/
     private GitBranchData gitBranchData;
 
-    private String CurrentBranch;
-    private String SelectedBranch;
 
     public GitBranchStatus(GitBranchData gitBranchData) {
         this.gitBranchData = gitBranchData;
 
-        CurrentBranch=gitBranchData.getCurrentBranch();
+        String CurrentBranch=gitBranchData.getCurrentBranch();
         JLabel printCurrentBranch=new JLabel(CurrentBranch);
-        SelectedBranch=gitBranchData.getSelectedBranch();
+        String SelectedBranch=gitBranchData.getSelectedBranch();
         JLabel printSelectedBranch=new JLabel(SelectedBranch);
+        setLayout(new BorderLayout(2,2));
+        add(printCurrentBranch);
+        add(printSelectedBranch);
 
-
-
-        add(printCurrentBranch, BorderLayout.NORTH);
-        add(printSelectedBranch,BorderLayout.CENTER);
 
     }
 }
--- src/main/java/gui/branchpanel/component/commit/GitCommitStatus.java
+++ src/main/java/gui/branchpanel/component/commit/GitCommitStatus.java
@@ -1,29 +1,53 @@
 package gui.branchpanel.component.commit;
 
 import file.GitBranchData;
+import jgitmanager.CommitInfo;
+import jgitmanager.JGitManagerImprv;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GitCommitStatus extends JPanel {
+    private JLabel CheckSum;
+    private JLabel CommitMessage;
+    private JLabel AuthorName;
+    private JLabel AuthorEmail;
+    private JLabel Date;
+    private CommitInfo commitInfo;
     private GitBranchData gitBranchData;
     public GitCommitStatus(GitBranchData gitBranchData){
+
         JPanel CommitDetailLabel=new JPanel(new GridLayout(0,1,2,2));
         this.add(CommitDetailLabel,BorderLayout.WEST);
 
         JPanel CommitDetailValue=new JPanel(new GridLayout(0,1,2,2));
         this.add(CommitDetailValue,BorderLayout.CENTER);
 
-        CommitDetailLabel.add(new JLabel("COMMIT",JLabel.TRAILING));
+        CommitDetailLabel.add(new JLabel("CHECKSUM",JLabel.TRAILING));
         /*COMMIT 내용 입력*/
-        CommitDetailLabel.add(new JLabel("TRACKING COMMIT",JLabel.TRAILING));
-        //TRACKING COMMIT 냬용
+        CheckSum=new JLabel(commitInfo.getCheckSum());
+        CommitDetailValue.add(CheckSum);
+
+        CommitDetailLabel.add(new JLabel("COMMIT MESSAGE",JLabel.TRAILING));
+        //COMMIT MESSAGE
+        CommitMessage=new JLabel(commitInfo.getCommitMessage());
+        CommitDetailValue.add(CommitMessage);
+
         CommitDetailLabel.add(new JLabel("AUTHOR",JLabel.TRAILING));
         //AUTHOR
+        AuthorName=new JLabel(commitInfo.getAuthorName());
+        CommitDetailValue.add(AuthorName);
+        CommitDetailLabel.add(new JLabel("AUTHOR EMAIL",JLabel.TRAILING));
+        //EMAIL
+        AuthorEmail=new JLabel(commitInfo.getAuthorEMail());
+        CommitDetailValue.add(AuthorEmail);
+
         CommitDetailLabel.add(new JLabel("DATE",JLabel.TRAILING));
+        Date=new JLabel(commitInfo.getCommitTime());
+        CommitDetailValue.add(Date);
         //DATE
-        CommitDetailLabel.add(new JLabel("USER",JLabel.TRAILING));
-        //USER
+
+
 
 
     }
