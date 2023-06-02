5d4971dbed55b3fd5b529cffb61e62a70bdb3ff4
keylime7 <keylime7@cau.ac.kr>
2023-06-02 20:15:20 +0900

    apapt event: file diff : diff print error

--- src/main/java/gui/branchpanel/component/commit/GitCommitModify.java
+++ src/main/java/gui/branchpanel/component/commit/GitCommitModify.java
@@ -1,5 +1,6 @@
 package gui.branchpanel.component.commit;
 
+import file.BranchDataChangeListener;
 import file.GitBranchData;
 import file.SelectedFile;
 import jgitmanager.CommitInfo;
@@ -8,20 +9,34 @@ import jgitmanager.JGitManagerImprv;
 import javax.swing.*;
 import java.awt.*;
 
-public class GitCommitModify extends JPanel {
+public class GitCommitModify extends JPanel implements BranchDataChangeListener {
     private String Diff;
     private JTextArea Differences;
     private GitBranchData gitBranchData;
     private CommitInfo commitInfo;
+
     public GitCommitModify(GitBranchData gitBranchData){
+        this.gitBranchData = gitBranchData;
+        gitBranchData.addBranchDataChangeListener(this);
+
+        Differences=new JTextArea();
+        this.add(Differences, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void updateData() {
         try {
             Diff = JGitManagerImprv.gitDiff(SelectedFile.getInstance().getFile(),
                     gitBranchData.getSelectedCommit(), gitBranchData.getSelectedChangeFile());
+            System.out.println("Test1: " + SelectedFile.getInstance().getFile().getName());
+            System.out.println("Test2: " + gitBranchData.getSelectedCommit().getName());
+            System.out.println("Test3: " + gitBranchData.getSelectedChangeFile().getName());
+            System.out.println("Test4: " + gitBranchData.getSelectedChangeFile().getAbsolutePath());
         }catch (Exception e){
             System.out.println(e.toString());
         }
-        Differences=new JTextArea(Diff);
-        this.add(Differences, BorderLayout.CENTER);
 
+        System.out.println("Test Diff: " + Diff);
+        Differences.setText(Diff);
     }
 }
