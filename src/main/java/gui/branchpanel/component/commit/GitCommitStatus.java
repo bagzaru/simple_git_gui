bcfe7781310502540b4e078bf334c89cc74f8a99
bagzaru <bagzaru@naver.com>
2023-06-09 18:39:40 +0900

    commit status changed as GridLayout

--- src/main/java/gui/branchpanel/component/commit/GitCommitStatus.java
+++ src/main/java/gui/branchpanel/component/commit/GitCommitStatus.java
@@ -16,8 +16,11 @@ public class GitCommitStatus extends JPanel implements BranchDataChangeListener 
     private JLabel Date;
     private CommitInfo commitInfo;
     private GitBranchData gitBranchData;
+    private JPanel CommitDetailLabel;
+    private JPanel CommitDetailValue;
 
     public GitCommitStatus(GitBranchData gitBranchData){
+        super(new GridLayout(1, 2));
         this.gitBranchData = gitBranchData;
         gitBranchData.addCurrentBranchChangeEventListeners(this);
         gitBranchData.addCommitSelectionEventListeners(this);
@@ -25,35 +28,35 @@ public class GitCommitStatus extends JPanel implements BranchDataChangeListener 
         gitBranchData.addFileSelectionEventListeners(this);
         gitBranchData.addGitBranchCommandEventListener(this);
 
-        JPanel CommitDetailLabel=new JPanel(new GridLayout(0,1,2,2));
-        JPanel CommitDetailValue=new JPanel(new GridLayout(0,1,2,2));
+        CommitDetailLabel=new JPanel(new GridLayout(0,1,2,2));
+        CommitDetailValue=new JPanel(new GridLayout(0,1,2,2));
 
-        CommitDetailLabel.add(new JLabel("CHECKSUM",JLabel.TRAILING));
+        CommitDetailLabel.add(new JLabel("CHECKSUM   ",JLabel.TRAILING));
         /*COMMIT 내용 입력*/
         CheckSum=new JLabel();
         CommitDetailValue.add(CheckSum);
 
-        CommitDetailLabel.add(new JLabel("COMMIT MESSAGE",JLabel.TRAILING));
+        CommitDetailLabel.add(new JLabel("COMMIT MESSAGE   ",JLabel.TRAILING));
         //COMMIT MESSAGE
         CommitMessage=new JLabel();
         CommitDetailValue.add(CommitMessage);
 
-        CommitDetailLabel.add(new JLabel("AUTHOR",JLabel.TRAILING));
+        CommitDetailLabel.add(new JLabel("AUTHOR   ",JLabel.TRAILING));
         //AUTHOR
         AuthorName=new JLabel();
         CommitDetailValue.add(AuthorName);
-        CommitDetailLabel.add(new JLabel("AUTHOR EMAIL",JLabel.TRAILING));
+        CommitDetailLabel.add(new JLabel("AUTHOR EMAIL   ",JLabel.TRAILING));
         //EMAIL
         AuthorEmail=new JLabel();
         CommitDetailValue.add(AuthorEmail);
 
-        CommitDetailLabel.add(new JLabel("DATE",JLabel.TRAILING));
+        CommitDetailLabel.add(new JLabel("DATE   ",JLabel.TRAILING));
         Date=new JLabel();
         CommitDetailValue.add(Date);
-        //DATE
 
-        this.add(CommitDetailLabel,BorderLayout.WEST);
-        this.add(CommitDetailValue,BorderLayout.CENTER);
+        //왼쪽 부분 길이 지정
+        this.add(CommitDetailLabel);
+        this.add(CommitDetailValue);
     }
 
     @Override
@@ -69,13 +72,34 @@ public class GitCommitStatus extends JPanel implements BranchDataChangeListener 
         else
             CheckSum.setText(commitInfo.getCheckSum());
 
-        if(commitInfo.getCommitMessage().length() > 30)
-            CommitMessage.setText(commitInfo.getCommitMessage().substring(0, 30) + "...");
-        else
-            CommitMessage.setText(commitInfo.getCommitMessage());
+        CommitMessage.setText(commitInfo.getCommitMessage());
 
         AuthorName.setText(commitInfo.getAuthorName());
         AuthorEmail.setText(commitInfo.getAuthorEMail());
         Date.setText(commitInfo.getCommitTime());
+    }
+
+    public String getAdaptiveText(JPanel parentPanel, JLabel label) {
+        int panelWidth = parentPanel.getWidth();
+        String labelText = label.getText();
+        Font labelFont = label.getFont();
+        FontMetrics labelFontMetrics = label.getFontMetrics(labelFont);
+
+        int horizontalPadding = 5; //좌우 여백 길이(pixel)
+        int availableWidth = panelWidth - 2 * horizontalPadding; //실제 가능한 좌우 길이
+        int textWidth = labelFontMetrics.stringWidth(labelText);    //Label에 적용되는 텍스트의 좌우길이를 가져옴
+
+        System.out.println("텍스트: "+labelText+", 가용길이: "+availableWidth);
+
+        //만약 가용길이보다 더 길다면 잘라냄
+        if (textWidth > availableWidth) {
+            int preferredTextWidth = availableWidth - 2 * labelFontMetrics.stringWidth("...");
+            String trimmedText = labelText;
+            while (labelFontMetrics.stringWidth(trimmedText) > preferredTextWidth&&trimmedText.length()>=1) {
+                trimmedText = trimmedText.substring(0, trimmedText.length() - 1);
+            }
+            labelText = trimmedText + "...";
+        }
+        return labelText;
     }
 }
